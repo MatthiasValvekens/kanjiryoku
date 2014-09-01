43a35a3a132bfef2769bc70ee4cbab562dbabb4c
Matthias Valvekens <matthias.valvekens@gmail.com>
2014-09-01 19:42:23 +0200

    First commit.

---
+++ src/main/java/be/mapariensis/kanjiryoku/model/Word.java
@@ -0,0 +1,43 @@
+package be.mapariensis.kanjiryoku.model;
+
+
+public class Word {
+	public final String furigana;
+	public final String main;
+	public Word(String main,String furigana) {
+		this.furigana = furigana != null ? furigana : "";
+		if(main == null) throw new IllegalArgumentException("Main part must not be null");
+		this.main = main;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((furigana == null) ? 0 : furigana.hashCode());
+		result = prime * result + ((main == null) ? 0 : main.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (furigana == null) {
+			if (other.furigana != null)
+				return false;
+		} else if (!furigana.equals(other.furigana))
+			return false;
+		if (main == null) {
+			if (other.main != null)
+				return false;
+		} else if (!main.equals(other.main))
+			return false;
+		return true;
+	}
+	
+}
