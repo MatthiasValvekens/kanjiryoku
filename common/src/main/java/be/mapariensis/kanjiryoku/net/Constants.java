e3ee40267e53f643517f8009ae60adb876c160ae
Matthias Valvekens <matthias.valvekens@gmail.com>
2015-03-10 09:57:36 +0100

    Bumped protocol minor version

--- common/src/main/java/be/mapariensis/kanjiryoku/net/Constants.java
+++ common/src/main/java/be/mapariensis/kanjiryoku/net/Constants.java
@@ -6,7 +6,7 @@ import be.mapariensis.kanjiryoku.net.commands.ServerCommandList;
 
 public class Constants {
 	public static final int protocolMajorVersion = 1;
-	public static final int protocolMinorVersion = 0;
+	public static final int protocolMinorVersion = 1;
 	public static final Charset ENCODING = Charset.forName("UTF-8");
 	public static final String GREETING = "Kanjiryoku server says hello.\n"
 			+ "Registration command is " + ServerCommandList.REGISTER
