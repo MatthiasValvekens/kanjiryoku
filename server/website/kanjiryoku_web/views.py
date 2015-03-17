bd0af55b17fd8a6160d8849fe95d307ccf9910dd
Matthias Valvekens <matthias.valvekens@gmail.com>
2015-03-17 18:53:34 +0100

    Added views/urls for signing up

--- server/website/kanjiryoku_web/views.py
+++ server/website/kanjiryoku_web/views.py
@@ -1,1 +1,17 @@
+from django.views.generic.edit import CreateView
+from kanjiryoku_web.models import KanjiUser
+from kanjiryoku_web.forms import KanjiryokuSignupForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
 
+class SignupView(CreateView):
+	model = KanjiUser
+	form_class = KanjiryokuSignupForm
+	def get_success_url(self):
+		return reverse('success_url') 	
+	def form_valid(self, form):
+		form.instance.django_user = self.request.user
+		return super(SignupView, self).form_valid(form)
+
+def success(request):
+	return render_to_response('kanjiryoku_web/success.html')
