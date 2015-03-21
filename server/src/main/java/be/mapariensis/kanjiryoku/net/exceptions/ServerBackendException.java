e0525120c7a9490077288b99dad1a204aba970db
Matthias Valvekens <matthias.valvekens@gmail.com>
2015-03-21 22:22:27 +0100

    Added extra constructor to ServerBackendException class

--- server/src/main/java/be/mapariensis/kanjiryoku/net/exceptions/ServerBackendException.java
+++ server/src/main/java/be/mapariensis/kanjiryoku/net/exceptions/ServerBackendException.java
@@ -4,4 +4,9 @@ public class ServerBackendException extends ServerException {
 	public ServerBackendException(Throwable cause) {
 		super("Internal server error. Page the admin.", cause, ERROR_BACKEND);
 	}
+
+	public ServerBackendException(String message) {
+		super("Internal server error. Page the admin.", new RuntimeException(
+				message), ERROR_BACKEND);
+	}
 }
